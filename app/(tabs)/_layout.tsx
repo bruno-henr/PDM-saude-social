6260b5b18bf22a2400158eb700dc1087a8481ef0
Bruno henrique <brunohenriquex0@gmail.com>
2025-03-13 18:52:57 -0300

    envolvendo searchcontexto na aplicacao

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,39 +7,44 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBar } from '@/components/BottomTab';
+import { CustomHeader } from '@/components/HeaderCustom';
+import { SearchProvider } from '@/context/SeachContext';
+import { View } from 'react-native';
+import Search from './search';
+import { Feather } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <SearchProvider>
+      <Tabs
+        tabBar={props => <TabBar {...props} />}
+        screenOptions={({ navigation, route }) => ({
+          header: () => <CustomHeader navigation={navigation} route={route} />,
+        })}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Inicio',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: 'Pesquisar',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="post"
+          options={{
+            title: 'Criar',
+            tabBarIcon: ({ color }) => <Feather size={28} name="plus" color={color} />,
+          }}
+        />
+      </Tabs>
+    </SearchProvider>
   );
 }
